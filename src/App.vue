8a98f068f79bdcc01c52a4f999dd0000d423fcf4
akamyratsapar <saparmyrat.myradov@bil.omu.edu.tr>
2021-08-12 23:10:40 +0300

    Updates

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif, Consolas;
+  font-family: Avenir, Helvetica, Arial, sans-serif, Arial;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
